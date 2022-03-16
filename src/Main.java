b391d5c5b7ee63ba8ecaca85fba9f49a6b92a62b
Plushcake <99784465+Plushcake@users.noreply.github.com>
2022-03-17 02:19:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         if (equation >= 1200) {
             equation = enterMoney + clientMoney + bonusMoney; // С 9 по 20 строчку была использованна магия.
         } else {                                              // Сам не понял какая. Но каставал очень много часов. @_@.
-            equation = enterMoney + clientMoney;              // Повтарить смогу, наверное.
+            equation = enterMoney + clientMoney;              // Повторить смогу, наверное.
         }
 
 
